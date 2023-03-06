93295883c6b2d2109277ec3fd2dafb0357b9481e
auramel <auramel@yandex.ru>
2023-03-06 16:32:01 +0500

    f

--- app/Models/TgHashRoute.php
+++ app/Models/TgHashRoute.php
@@ -4,6 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @mixin IdeHelperTgHashRoute
+ */
 class TgHashRoute extends Model
 {
     public $table = 'tg_hash_routes';
--- app/Models/TgUser.php
+++ app/Models/TgUser.php
@@ -4,6 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @mixin IdeHelperTgUser
+ */
 class TgUser extends Model
 {
     public $table = 'tg_users';
